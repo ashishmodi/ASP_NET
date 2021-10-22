b6001d7d0ca7b23690b071ccaa968b0f02ff8738
gashishmukesh92 <gashishmkesh92@gmail.com>
2021-10-22 20:53:35 +0530

    Application_Start, Session_Start demo

---
+++ ASP_NET_Learnings/Basics/ApplicationNSessionsDemo.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Basics
+{
+    public partial class ApplicationNSessionsDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("No of applications = " + Application["TotalApp"].ToString());
+            Response.Write("<br>");
+            Response.Write("No of users online = " + Application["TotalUser"].ToString());
+        }
+    }
+}
